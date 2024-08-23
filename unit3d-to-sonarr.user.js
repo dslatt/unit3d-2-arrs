2bbcc4428cbdcf8bd8406383f05fe3d74a7fdd94
Maksym Martynov <1761348+maksii@users.noreply.github.com>
2024-08-23 11:22:33 +0300

    version up

--- unit3d-to-sonarr.user.js
+++ unit3d-to-sonarr.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         UNIT3D-to-Sonarr
-// @version      0.2
+// @version      0.2.1
 // @author       dantayy
 // @namespace    https://github.com/frenchcutgreenbean/
 // @description  Send series to sonarr from UNIT3D trackers
